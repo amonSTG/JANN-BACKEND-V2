05dbe9f9870bc68d42ba9d6bfaaf8c98b20a442e
AndyMondri <Andy.Mondri@brightstraining.com>
2022-12-20 13:45:46 +0100

    Working Backend
    + added GetMapping

--- src/main/java/com/jannbackendv2/MainController.java
+++ src/main/java/com/jannbackendv2/MainController.java
@@ -3,7 +3,11 @@ package com.jannbackendv2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.LinkedList;
+import java.util.List;
+
 @RestController
+@CrossOrigin(origins = "*")
 public class MainController {
 
     private final EntryRepository entryRepository;
@@ -12,13 +16,17 @@ public class MainController {
         this.entryRepository = entryRepository;
     }
 
-    @CrossOrigin(origins = "*")
-    @GetMapping("/")
-    public String getMethod(){
-        return null;
+    @GetMapping("/api/results")
+    public List<EntryDTO> read() {
+        List<EntryDTO> response = new LinkedList<>();
+
+        for (Entry entry : entryRepository.findAllByOrderByCreatedAtDesc()) {
+            response.add(new EntryDTO(entry.get_name(), entry.get_score(), entry.get_time()));
+        }
+
+        return response;
     }
 
-    @CrossOrigin(origins = "*")
     @PostMapping("/api/score")
     public EntryDTO write(@RequestBody EntryDTO entryDTO) {
         Entry entry = entryRepository.save(new Entry(entryDTO.get_name(), entryDTO.get_score(), entryDTO.get_time()));
